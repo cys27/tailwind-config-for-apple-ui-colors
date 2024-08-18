4b7cbbf527c0c1ef2e34fa94c4a4164cb80cf94a
enes <139135923+cys27@users.noreply.github.com>
2024-08-18 19:22:25 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,78 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // red
+        "macos-red-default-light": "rgb(255,59,48)",
+        "macos-red-default-dark": "rgb(255,69,58)",
+        "macos-red-accessible-light": "rgb(215,0,21)",
+        "macos-red-accessible-dark": "rgb(255,105,97)",
+        // orange
+        "macos-orange-default-light": "rgb(255,149,0)",
+        "macos-orange-default-dark": "rgb(255,159,10)",
+        "macos-orange-accessible-light": "rgb(201,52,0)",
+        "macos-orange-accessible-dark": "rgb(255,179,64)",
+        // yellow
+        "macos-yellow-default-light": "rgb(255,204,0)",
+        "macos-yellow-default-dark": "rgb(255,214,10)",
+        "macos-yellow-accessible-light": "rgb(160,90,0)",
+        "macos-yellow-accessible-dark": "rgb(255,212,38)",
+        // green
+        "macos-green-default-light": "rgb(40,205,55)",
+        "macos-green-default-dark": "rgb(50,215,75)",
+        "macos-green-accessible-light": "rgb(0,125,27)",
+        "macos-green-accessible-dark": "rgb(49,222,75)",
+        // mint
+        "macos-mint-default-light": "rgb(0,199,190)",
+        "macos-mint-default-dark": "rgb(102,212,207)",
+        "macos-mint-accessible-light": "rgb(12,129,123)",
+        "macos-mint-accessible-dark": "rgb(102,212,207)",
+        // teal
+        "macos-teal-default-light": "rgb(89,173,196)",
+        "macos-teal-default-dark": "rgb(106,196,220)",
+        "macos-teal-accessible-light": "rgb(0,130,153)",
+        "macos-teal-accessible-dark": "rgb(93,230,255)",
+        // cyan
+        "macos-cyan-default-light": "rgb(85,190,240)",
+        "macos-cyan-default-dark": "rgb(90,200,245)",
+        "macos-cyan-accessible-light": "rgb(0,113,164)",
+        "macos-cyan-accessible-dark": "rgb(112,215,255)",
+        // blue
+        "macos-blue-default-light": "rgb(0,122,255)",
+        "macos-blue-default-dark": "rgb(10,132,255)",
+        "macos-blue-accessible-light": "rgb(0,64,221)",
+        "macos-blue-accessible-dark": "rgb(64,156,255)",
+        // indigo
+        "macos-indigo-default-light": "rgb(88,86,214)",
+        "macos-indigo-default-dark": "rgb(94,92,230)",
+        "macos-indigo-accessible-light": "rgb(54,52,163)",
+        "macos-indigo-accessible-dark": "rgb(125,122,255)",
+        // purple
+        "macos-purple-default-light": "rgb(175,82,222)",
+        "macos-purple-default-dark": "rgb(191,90,242)",
+        "macos-purple-accessible-light": "rgb(173,68,171)",
+        "macos-purple-accessible-dark": "rgb(218,143,255)",
+        // pink
+        "macos-pink-default-light": "rgb(255,45,85)",
+        "macos-pink-default-dark": "rgb(255,55,95)",
+        "macos-pink-accessible-light": "rgb(211,15,69)",
+        "macos-pink-accessible-dark": "rgb(255,100,130)",
+        // brown
+        "macos-brown-default-light": "rgb(162,132,94)",
+        "macos-brown-default-dark": "rgb(172,142,104)",
+        "macos-brown-accessible-light": "rgb(127,101,69)",
+        "macos-brown-accessible-dark": "rgb(181,148,105)",
+        // gray
+        "macos-gray-default-light": "rgb(142,142,147)",
+        "macos-gray-default-dark": "rgb(152,152,157)",
+        "macos-gray-accessible-light": "rgb(105,105,110)",
+        "macos-gray-accessible-dark": "rgb(152,152,157)",
+      }
+    },
+  },
+  plugins: [],
+}
